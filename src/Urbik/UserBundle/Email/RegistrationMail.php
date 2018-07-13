4954b5b40b8f838944c381e28770ac4c5a5ec9ed
frenchincubus <lionelrenier@sfr.fr>
2018-07-13 13:51:56 +0200

    new commit

--- src/Urbik/UserBundle/Email/RegistrationMail.php
+++ src/Urbik/UserBundle/Email/RegistrationMail.php
@@ -29,22 +29,22 @@ class RegistrationMail
         $admin = $this->doctrine->getEntityManager()->getRepository('UrbikAuthBundle:Admin')->findAll();
 
         $message = (new \Swift_Message('Confirmation d\'inscription '.$user->getNom().' '.$user->getPrenom()))
-                    ->setFrom(['misterbluepearl@hotmail.com' => 'Urbik Admin'])
+                    ->setFrom(['kibru-966@outlook.fr' => 'Urbik Admin'])
                     ->setTo($user->getMail())
                     ->setBcc($admin[0]->getEmail())
                     ->setBody(
                         '<h3>Message de confirmation</h3>
 
-                        <p>Hello '.$user->getNom().' '. $user->getPrenom().' , your account is successfully registered.</p>
-                        <p>Your informations :</p> 
+                        <p>Hello '.$user->getNom().' '. $user->getPrenom().' , your account is successfully registered.</p><br>
+                        <p>Your informations :</p><br>
                         
-                        <p>'.$user->getBirthdate()->format('d M Y').'</p>
-                        <p>'.$user->getSex().'</p>
-                        <p>'.$user->getCountry().'</p>
-                        <p>'.$user->getRegion().'</p>
-                        <p>'.$user->getJob().'</p>
+                        <p><strong>Birthdate</strong>: '.$user->getBirthdate()->format('d M Y').'</p>
+                        <p><strong>Sex</strong>: '.$user->getSex().'</p>
+                        <p><strong>Country</strong>: '.$user->getCountry().'</p>
+                        <p><strong>Region</strong>: '.$user->getRegion().'</p>
+                        <p><strong>Job</strong>: '.$user->getJob().'</p>
                         
-                        <p>Thanks.</p>',
+                        <br><p>Thanks.</p>',
                         'text/html'
                     );
 
